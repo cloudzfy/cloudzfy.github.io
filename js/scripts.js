688bf5cff995574e08e059467513103e367e83ff
cloudzfy <cloudzfy@gmail.com>
2014-01-23 22:28:11 +0800

    Reconstruct personal website.

---
+++ js/scripts.js
@@ -0,0 +1,112 @@
+$(function () {
+	
+	$("#cover").css("height", $(window).height());
+	
+	$(window).resize(function() {
+		$("#cover").css("height", $(window).height());
+		$("body").scrollspy("refresh");
+	});
+	
+	$('.flexslider').flexslider({
+		animation: "slide",
+		slideshow: true,
+		slideshowSpeed: 3000,
+		animationSpeed: 600,
+		pauseOnHover: true
+	});
+	
+	var project_preview = $("#project-preview .container");
+	
+	$("a.project-info").click(function () {
+		
+		$("#project-preview").css("height", "auto");
+		$("#progress-bar").show();
+		progress(100, $("#progress-bar"));
+		
+		var url = "projects/" + $(this).attr("name") + ".html?" + getRandom(10000);
+		$("#project-preview").css("height", "auto");
+		$.get(url, function(data) {
+			project_preview.html(data);
+			$("html, body").animate({scrollTop: $("#project-preview").offset().top - 60}, 500, function() {
+				$("body").scrollspy("refresh");
+			});
+			$(".preview-close").click(function() {
+                $("#project-preview").animate({height: "0"}, 800);
+				$("body").animate({scrollTop: $("#projects").offset().top - 60}, 800, function() {
+					$("body").scrollspy("refresh");
+				});
+            });
+			$("#progress-bar").fadeOut("slow");
+			$(".flexslider").flexslider();
+		});
+	});
+	
+	var publication_preview = $("#publication-preview .container");
+	
+	$("a.publication-info").click(function () {
+		
+		$("#publication-preview").css("height", "auto");
+		$("#progress-bar").show();
+		progress(100, $("#progress-bar"));
+		
+		var url = "publications/" + $(this).attr("name") + ".html?" + getRandom(10000);
+		$("#publication-preview").css("height", "auto");
+		$.get(url, function(data) {
+			publication_preview.html(data);
+			$("html, body").animate({scrollTop: $("#publication-preview").offset().top - 60}, 500, function() {
+				$("body").scrollspy("refresh");
+			});
+			$(".preview-close").click(function() {
+                $("#publication-preview").animate({height: "0"}, 800);
+				$("body").animate({scrollTop: $("#publications").offset().top - 60}, 800, function() {
+					$("body").scrollspy("refresh");
+				});
+            });
+			$("#progress-bar").fadeOut("slow");
+		});
+	});
+	
+	function progress(percent, $element) {
+        var progressBarWidth = percent * $element.width() / 100;
+        $element.find('div').animate({ width: progressBarWidth }, 1200);
+	}
+	
+	$('.navbar a[href^="#"]').bind('click', function (event) {
+    	event.preventDefault();
+		isTop = false;
+    	var target = this.hash;
+        var anchor = $(this);
+
+        $("html, body").stop().animate({
+            "scrollTop" : $(anchor.attr('href')).offset().top - 60
+        }, 500, "swing");
+    });
+	
+	var isTop = true;
+	var previous = 0;
+	
+	$(window).scroll(function(event) {
+		event.stopPropagation();
+		event.preventDefault();
+		var current = $(window).scrollTop();
+		if(current < 0) {
+			return;
+		}
+		var target = this.hash;
+		if(isTop == true && previous < current) {
+			isTop = false;
+			$("html, body").stop().animate({
+				"scrollTop": $("#projects").offset().top - 60
+			}, 400, "swing");
+		}
+		else if (current == 0) {
+			isTop = true;
+		}
+		previous = current;
+	});
+	
+	function getRandom(n) {
+		return Math.floor(Math.random() * n + 1)
+	}
+	
+});
